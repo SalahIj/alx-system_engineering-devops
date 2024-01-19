91fb06480ee968cec5aa041b33bfc452d75e26ed
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-19 20:23:55 +0000

    Task_1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,5 @@
-# 1-install_a_package.pp
-
-# Install python3 and pip3
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Ensure the package is present and at the specified version
-package { 'Flask':
+# A Manifest that's installs Flask v2.1.0 on Puppet-agent.
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => [Package['python3'], Package['python3-pip']],  # Ensure python3 and pip3 are installed first
+  provider => pip3,
 }
