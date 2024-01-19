94e67a385676cb569f6cfc6c34e7267da43f85bf
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-19 19:38:47 +0000

    Task_0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Files creating
 
 file { '/tmp/school':
-  content => 'I love Puppet'
   ensure  => file,
+  content => 'I love Puppet',
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
