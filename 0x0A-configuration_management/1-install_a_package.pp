3fe26cec7845237eaa984767068f4f674ecbfb04
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-19 20:21:33 +0000

    Task_1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# Install flask v2.1.0 from pip3
-package { 'flask':
+# 1-install_a_package.pp
+
+# Install python3 and pip3
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Ensure the package is present and at the specified version
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => [Package['python3'], Package['python3-pip']],  # Ensure python3 and pip3 are installed first
 }
