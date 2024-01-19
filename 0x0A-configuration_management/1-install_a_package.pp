a6862ecc5d7479eff5365c93351d7531e2adaffb
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-19 20:12:09 +0000

    Task_1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,5 +4,5 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }
