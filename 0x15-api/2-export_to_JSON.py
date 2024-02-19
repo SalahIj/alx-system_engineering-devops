e9244ad5e8b84d1e82ae372362bdbf8841b992a9
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-19 22:05:49 +0000

    Task_2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,7 +6,8 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def main():
     url = "https://jsonplaceholder.typicode.com"
     Id_user = sys.argv[1]
     user = requests.get("{}/users/{}".format(url, Id_user))
@@ -19,14 +20,18 @@ if __name__ == "__main__":
     for task in tasks:
         if task.get("userId") == int(Id_user):
             Title = task.get("title")
-            Completed = task.get('completed')
-            Username = user.json().get('username')
+            Completed = task.get("completed")
+            Username = user.json().get("username")
             Dict_of_task = {"task": Title,
                             "completed": Completed,
                             "username": Username}
             List_of_task.append(Dict_of_task)
     User_to_do[Id_user] = List_of_task
 
-    filename = Id_user + '.json'
-    with open(filename, mode='w') as file:
+    filename = Id_user + ".json"
+    with open(filename, mode="w") as file:
         json.dump(User_to_do, file)
+
+
+if __name__ == "__main__":
+    main()
