56f30cc9dbb986623939ebc29c1afc57ae595517
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-03-07 16:15:27 +0000

    Task_1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,16 @@ def top_ten(subreddit):
     """ The function definition """
     try:
         headers = {"User-Agent": "My-User-Agent"}
-        response = requests.get("https://www.reddit.com/r/{}/hot.json".
-                                format(subreddit),
-                                headers=headers,
-                                allow_redirects=False)
-        if (response.status_code == 404):
-            print('None')
+        resp = requests.get("https://www.reddit.com/r/{}/hot.json".
+                            format(subreddit),
+                            headers=headers,
+                            allow_redirects=False)
+        if resp.status_code == 404:
+            print("None")
             return 0
-        request = response.json().get('data').get('children')
+        request = resp.json().get("data").get("children")
         for i in range(10):
-            print(request[i].get('data').get('title'))
+            print(request[i].get("data").get("title"))
     except Exception:
-        print('None')
+        print("None")
         return 0
