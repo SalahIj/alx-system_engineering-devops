4dfa5d38b222232f2e5152211b67623f05d37544
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-03-07 15:04:36 +0000

    Task_3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     """
     import requests
 
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    sub_info = requests.get(URL,
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
