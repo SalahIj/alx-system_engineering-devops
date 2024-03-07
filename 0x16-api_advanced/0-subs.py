83117e0e0c850f6c274aaba2aae92408a585b8b5
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-03-07 15:15:47 +0000

    Task_3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """____________________________________________________________
+    ----------------"""
 
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     Agent_user = {"User-Agent": "My-User-Agent"}
-    sub_info = requests.get(URL, headers=Agent_user,
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    Groupe_info = requests.get(URL, headers=Agent_user, allow_redirects=False)
+    if Groupe_info.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    return Groupe_info.json().get("data").get("subscribers")
