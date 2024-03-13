222398a1f492b82f62bb40d86195f32617324f1e
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-03-13 11:20:01 +0000

    Task_0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Change the limit from 15 to 2000
+exec { 'set limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/15/2000/' /etc/default/nginx"
+}
+exec { 'reboot nginx':
+  command => '/usr/sbin/service nginx restart'
+}
